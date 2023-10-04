789b74288bbc7b29d6d575b395bb09745b088415
edixon.rojas <edixon.rojas@tcs.com.ve>
2023-10-04 10:25:40 -0400

    Segundo metodo de combos

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,10 +1,15 @@
 const StudentController = {};
 const {Student} = require('../db/sequelize');
 const {EducationalGradeAttend} = require('../db/sequelize');
+const {EducationalLevel} = require('../db/sequelize');
 const response = require('../utils/response');
 
 function findAllEducationalGradeAttend(){
     return EducationalGradeAttend.findAll();
+}
+
+function findAllEducationLevel(){
+    return EducationalLevel.findAll();
 }
 
 StudentController.getAllEducationalGradeAttend = (req, res) => {
@@ -13,12 +18,30 @@ StudentController.getAllEducationalGradeAttend = (req, res) => {
         if(educationalGradeAttend == null){
             res.json(response({
                 status: 'ERROR',
-                msg: 'Niveles educativos no disponibles'
+                msg: 'Niveles educativos a cursar no disponibles'
             }));
         } else {
             res.json(response({
                 status: 'SUCCESS',
                 data: educationalGradeAttend,
+            }));
+        }
+
+    })
+}
+
+StudentController.getAllEducationalLevel = (req, res) => {
+    findAllEducationLevel().then(educationalLevel => {
+
+        if(educationalLevel == null){
+            res.json(response({
+                status: 'ERROR',
+                msg: 'Niveles educativos no disponibles'
+            }));
+        } else {
+            res.json(response({
+                status: 'SUCCESS',
+                data: educationalLevel,
             }));
         }
 
--- src/routes/StudentRoute.js
+++ src/routes/StudentRoute.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const StudentController = require('../controllers/StudentController');
 
-router.route('/getAllEducationalLevelAttend')
+router.route('/getAllEducationalGradeAttend')
     .get(StudentController.getAllEducationalGradeAttend);
 
+router.route('/getAllEducationalLevel')
+    .get(StudentController.getAllEducationalLevel);
 
 
 module.exports = router;
