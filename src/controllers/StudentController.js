234ac9e5f7bf23f2440d3324d0eb149f97cd52e5
edixon.rojas <edixon.rojas@tcs.com.ve>
2023-10-05 10:04:16 -0400

    Return de los combos relacionados a los usuarios

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -2,6 +2,11 @@ const StudentController = {};
 const {Student} = require('../db/sequelize');
 const {EducationalGradeAttend} = require('../db/sequelize');
 const {EducationalLevel} = require('../db/sequelize');
+const {Roles} = require('../db/sequelize');
+const {Gender} = require('../db/sequelize');
+const {CivilStatus} = require('../db/sequelize');
+const {WorkingCondition} = require('../db/sequelize');
+const {IncomeLevel} = require('../db/sequelize');
 const response = require('../utils/response');
 
 function findAllEducationalGradeAttend(){
@@ -10,6 +15,26 @@ function findAllEducationalGradeAttend(){
 
 function findAllEducationLevel(){
     return EducationalLevel.findAll();
+}
+
+function findAllRoles(){
+    return Roles.findAll();
+}
+
+function findAllGender(){
+    return Gender.findAll();
+}
+
+function findAllCivilStatus(){
+    return CivilStatus.findAll();
+}
+
+function findAllWorkingCondition(){
+    return WorkingCondition.findAll();
+}
+
+function findAllIncomeLevel(){
+    return IncomeLevel.findAll();
 }
 
 StudentController.getAllEducationalGradeAttend = (req, res) => {
@@ -42,6 +67,96 @@ StudentController.getAllEducationalLevel = (req, res) => {
             res.json(response({
                 status: 'SUCCESS',
                 data: educationalLevel,
+            }));
+        }
+
+    })
+}
+
+StudentController.getAllRoles = (req, res) => {
+    findAllRoles().then(roles => {
+
+        if(roles == null){
+            res.json(response({
+                status: 'ERROR',
+                msg: 'Roles no disponibles'
+            }));
+        } else {
+            res.json(response({
+                status: 'SUCCESS',
+                data: roles,
+            }));
+        }
+
+    })
+}
+
+StudentController.getAllGender = (req, res) => {
+    findAllGender().then(gender => {
+
+        if(gender == null){
+            res.json(response({
+                status: 'ERROR',
+                msg: 'Generos no disponibles'
+            }));
+        } else {
+            res.json(response({
+                status: 'SUCCESS',
+                data: gender,
+            }));
+        }
+
+    })
+}
+
+StudentController.getAllCivilStatus = (req, res) => {
+    findAllCivilStatus().then(civilStatus => {
+
+        if(civilStatus == null){
+            res.json(response({
+                status: 'ERROR',
+                msg: 'Estatus civiles no disponibles'
+            }));
+        } else {
+            res.json(response({
+                status: 'SUCCESS',
+                data: civilStatus,
+            }));
+        }
+
+    })
+}
+
+StudentController.getAllWorkingCondition = (req, res) => {
+    findAllWorkingCondition().then(workingCondition => {
+
+        if(workingCondition == null){
+            res.json(response({
+                status: 'ERROR',
+                msg: 'Condiciones laborales no disponibles'
+            }));
+        } else {
+            res.json(response({
+                status: 'SUCCESS',
+                data: workingCondition,
+            }));
+        }
+
+    })
+}
+
+StudentController.getAllIncomeLevel = (req, res) => {
+    findAllIncomeLevel().then(incomeLevel => {
+
+        if(incomeLevel == null){
+            res.json(response({
+                status: 'ERROR',
+                msg: 'Niveles de ingresos no disponibles'
+            }));
+        } else {
+            res.json(response({
+                status: 'SUCCESS',
+                data: incomeLevel,
             }));
         }
 
--- src/routes/StudentRoute.js
+++ src/routes/StudentRoute.js
@@ -7,5 +7,21 @@ router.route('/getAllEducationalGradeAttend')
 router.route('/getAllEducationalLevel')
     .get(StudentController.getAllEducationalLevel);
 
+router.route('/getAllRoles')
+    .get(StudentController.getAllRoles);
+
+router.route('/getAllGender')
+    .get(StudentController.getAllGender);
+
+router.route('/getAllCivilStatus')
+    .get(StudentController.getAllCivilStatus);
+
+router.route('/getAllWorkingCondition')
+    .get(StudentController.getAllWorkingCondition);  
+
+router.route('/getAllIncomeLevel')
+    .get(StudentController.getAllIncomeLevel);
+
+    
 
 module.exports = router;
